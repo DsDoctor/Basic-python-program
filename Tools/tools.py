c56cb40c51e60bb60f3f9fb40659d431c1676092
dsdoctor <ds_doctor@foxmail.com>
2020-05-08 21:03:23 +0800

    创建小工具用于打印进度

---
+++ Tools/tools.py
@@ -0,0 +1,14 @@
+"""
+小工具打印进度
+使用：
+    if __name__ == '__main__':
+        lis = range(777)
+        for i in lis:
+            print_progress(i, len(lis))
+            time.sleep(0.01)
+        print_progress(1, 1)
+        print('next')
+"""
+def print_progress(cur, tol):
+    print(f"\r|{''*(int((cur/tol)*20))}{' '*(20-int((cur/tol)*20))}| {cur/tol*100:.2f}%", end='')
+    if cur == tol: print()
